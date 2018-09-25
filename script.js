f291706927dc2d4337207c865ae226f92ead2bf4
Lnirmohi <laxmikant.nirmohi@gmail.com>
2018-09-25 20:16:09 +0530

    code refactored

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
+//predetermined results of game played and what beats what
+const objOfMatchReults = {
+    //played values - Winning value
+	'Rock Scissor' : 'Rock',
+	'Scissor Rock' : 'Rock',
+	'Paper Rock'   : 'Paper',
+	'Rock Paper'   : 'Paper',
+	'Scissor Paper': 'Scissor',
+	'Paper Scissor': 'Scissor'
+};
+
+let score = {
+    player: 0,
+    computer: 0
+};
+
 const keys = Array.from(document.querySelectorAll('.key'));
 
 keys.forEach( key =>  key.addEventListener('click', playGame));
 
+//delivers id of the selected element to game function
 function playGame(event) {
-    console.log(event.srcElement.id);
+    game(event.srcElement.id);
 }
 
-//game();
+function game(playerSelection) {
+     
+    //array of message to display and result
+    let msgAndResult = playRound(playerSelection, computerPlay());
 
+    renderAlert(msgAndResult[0]);
+
+    updateTable(msgAndResult[1]);
+}
+
+
+//return random selction 
 function computerPlay() {
     
     const gameValues = ['Rock', 'Paper', 'Scissor'];
@@ -15,74 +42,33 @@ function computerPlay() {
     return gameValues[Math.floor(Math.random() * 3)];
 }
 
+//returns an array of message to display and result of game
 function playRound(playerSelection, computerSelection) {
-    
-    
+
     if(playerSelection === computerSelection) {
         
-        alert(`It's a draw! You both drew ${computerSelection}`);
+        return [`It's a Draw! You both drew ${computerSelection}.`, 'draw'];
+    }else if(objOfMatchReults[playerSelection + " " + computerSelection] === playerSelection) {
         
-        return "draw";
-    }else if(playerSelection === 'rock') {
+        return [`You Won! ${playerSelection} beats ${computerSelection}.`,    'won'];
+    }else if(objOfMatchReults[playerSelection + " " + computerSelection] === computerSelection) {
         
-        (computerSelection === 'scissor') ? showAlert("You Won! Rock beats scissor") : showAlert("You Lose! Paper beats Rock");
-        
-        return (computerSelection === 'scissor') ? "won" : "lose";
-    }else if(playerSelection === 'paper') {
-        
-        (computerSelection === 'scissor') ? showAlert("You Won! Paper beats Rock") : showAlert("You Lose! Scissor beats Paper");
-        
-        return (computerSelection === 'rock') ? "won" :  "lose";
-    }else if(playerSelection === 'scissor') {
-        
-        (computerSelection === 'scissor') ? showAlert("You Won! Scissor beats Paper") : showAlert("You Lose! Rock beats Scissor");
-        
-        return (computerSelection == 'paper') ? "won" :  "lose";
+        return [`You Lose! ${computerSelection} beats ${playerSelection}.`,  'lose'];
     }
 }
 
-function showAlert(alertMsg) {
-    alert(alertMsg);
+function updateTable(outcome) {
+    
+    if(msgAndResult[1] != "draw"){
+        
+        msgAndResult[1] === "won" ? score.player++ : score.computer++;
+    }
 }
 
-function game() {
-    
-    let score = {
-        player: 0,
-        computer: 0
-    };
-    
-    for(let i = 1; i <= 5; i++) {
-        
-        let playerSelection;
-        
-        //loop will run untill player enter valid value i.e rock, paper or scissor
-        while(true) {
-            
-            playerSelection = prompt("Rock Paper Scissor!!!").toLowerCase();
-            
-            if(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissor"){
-                break;
-            }else {
-                alert("Invalid choice! Please play rock, paper or scissor");
-            }
-        }
-        
-        let computerSelection = computerPlay().toLowerCase();
-        
-        let result = playRound(playerSelection, computerSelection);
-        
-        if(result != "draw"){
-            result == "won" ? score.player++ : score.computer++;
-        }    
-    }
-    
-    console.log(`player: ${score.player}  ` , `computer: ${score.computer}`);
-    
-    if(score.player > score.computer)
-    console.log("Player won the game!");
-    else if(score.player < score.computer)
-    console.log("Computer won the game!");
-    else
-    console.log("The game is tie!");
+//renders msg on alert div of index
+function renderAlert(msg){
+
+    let alertBox = document.getElementById("alert-text");
+
+    alertBox.textContent = msg;
 }
