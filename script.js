f9fd17eb215972db8f0eea9c343b8a6349334dc6
Lnirmohi <laxmikant.nirmohi@gmail.com>
2018-09-28 00:10:02 +0530

    score and announcement reset added

--- script.js
+++ script.js
@@ -1,81 +1,78 @@
-//predetermined results of game played and what beats what
-const objOfMatchReults = {
-    //played values - Winning value
-	'Rock Scissor' : 'Rock',
-	'Scissor Rock' : 'Rock',
-	'Paper Rock'   : 'Paper',
-	'Rock Paper'   : 'Paper',
-	'Scissor Paper': 'Scissor',
-	'Paper Scissor': 'Scissor'
-};
-
 let score = {
     player: 0,
     computer: 0
 };
 
-const keys = Array.from(document.querySelectorAll('.key'));
+playerKeySelection();
 
-keys.forEach( key =>  key.addEventListener('click', playGame));
+function playerKeySelection() {
+    
+    const keys = Array.from(document.querySelectorAll('.key'));
 
-//delivers id of the selected element to game function
+    keys.forEach( key =>  key.addEventListener('click', playGame));
+}
+
+//delivers id (playerSelection) of the selected element to game function
 function playGame(event) {
     game(event.srcElement.id);
 }
 
 function game(playerSelection) {
      
-    //array of message to display and result
+    //Object of message to display and result
     let msgAndResult = playRound(playerSelection, computerPlay());
 
-    renderAlert(msgAndResult[0]);
+    //sending message in function
+    renderAlert(msgAndResult.message);
 
-    updateTable(msgAndResult[1]);
+    updateScores(msgAndResult.result);
 }
-
 
 //return random selction 
 function computerPlay() {
-    
-    const gameValues = ['Rock', 'Paper', 'Scissor'];
-    
-    return gameValues[Math.floor(Math.random() * 3)];
+
+    return ['Rock', 'Paper', 'Scissor'][Math.floor(Math.random() * 3)];
 }
 
-//returns an array of message to display and result of game
+//returns an array of message to display and result of single game
 function playRound(playerSelection, computerSelection) {
+
+    //predetermined results of game played and what beats what
+    const objOfMatchResults = {
+        //played values - Winning value
+        'Rock Scissor' : 'Rock',
+        'Scissor Rock' : 'Rock',
+        'Paper Rock'   : 'Paper',
+        'Rock Paper'   : 'Paper',
+        'Scissor Paper': 'Scissor',
+        'Paper Scissor': 'Scissor'
+    };
 
     if(playerSelection === computerSelection) {
         
-        return [`It's a Draw! You both drew ${computerSelection}.`, 'draw'];
-    }else if(objOfMatchReults[playerSelection + " " + computerSelection] === playerSelection) {
+        return {
+            message :`It's a Draw! You both drew ${computerSelection}.`, 
+            result : 'draw'
+        };
+    }else if(objOfMatchResults[playerSelection + " " + computerSelection] === playerSelection) {
         
-        return [`You Won! ${playerSelection} beats ${computerSelection}.`,    'won'];
-    }else if(objOfMatchReults[playerSelection + " " + computerSelection] === computerSelection) {
+        return {
+            message :`You Won! ${playerSelection} beats ${computerSelection}.`,
+            result : 'won'
+        };
+    }else if(objOfMatchResults[playerSelection + " " + computerSelection] === computerSelection) {
         
-        return [`You Lose! ${computerSelection} beats ${playerSelection}.`,  'lose'];
-    }
-}
-
-function updateTable(outcome) {
-
-    let scores = [document.getElementById("player"), document.getElementById("computer")];
-
-    if(outcome != "draw"){
-        
-        if(outcome === "won") {
-            scores[0].textContent = ++score.player;
-        }
-        else{
-            scores[1].textContent = ++score.computer;
-        }
+        return {
+            message : `You Lose! ${computerSelection} beats ${playerSelection}.`,
+            result : 'lose'
+        };
     }
 }
 
 //renders msg on alert div of index
-function renderAlert(msg){
+function renderAlert(msg) {
 
-    let alert = {
+    const alert = {
         box  : document.getElementById("alert-box"),
         text : document.getElementById("alert-text")
     };
@@ -85,4 +82,48 @@ function renderAlert(msg){
     alert.text.style.padding = "0 20px";
 
     alert.text.textContent = msg;
+}
+
+function updateScores(outcome) {
+
+    const scoreElements = {
+        playerElement : document.getElementById("player"),
+        computerElement : document.getElementById("computer")
+    }
+
+    if(outcome != "draw"){
+        
+        if(outcome === "won") {
+            scoreElements.playerElement.textContent = ++score.player;
+        }
+        else {
+            scoreElements.computerElement.textContent = ++score.computer;
+        }
+    }
+
+    if(score.player == 5 || score.computer == 5) {
+        checkForWinner();
+
+        initializeScores(scoreElements);
+
+        setTimeout( () => {
+            document.getElementById("announcement").textContent = "Shoot!";
+            document.getElementById("alert-box").style.visibility = "hidden";
+        },3000);
+    }
+}
+
+function checkForWinner() {
+
+    const announce = document.getElementById("announcement");
+
+    score.player == 5 ? announce.textContent = "Player won the game!" : announce.textContent = "Computer won the game!";
+}
+
+function initializeScores(scoreElements) {
+
+    score.player = score.computer = 0;
+    
+    scoreElements.playerElement.textContent = 0;
+    scoreElements.computerElement.textContent = 0;
 }
